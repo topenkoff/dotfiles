4067c052a78bfd88fade1b2802f70d0260894c58
Denis Kayshev <topenkoff@gmail.com>
2021-11-07 20:51:54 +0300

    upd nvim configs

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,19 @@
-local nvim_tree = require'nvim-tree'
-
-nvim_tree.setup{
-  open_on_tab         = true,
-  open_on_setup       = true,
+require'nvim-tree'.setup {
+  open_on_setup = true,
   view = {
-    width = 35,
+    width = 30,
     side = 'left',
-    auto_resize = true,
+    auto_resize = true,  
+  },
+  filters = {
+    custom = { 
+      '.git', 
+      'node_modules', 
+      '.nvimlog', 
+      '.idea', 
+      '__pycache__', 
+      '.venv' 
+    }
   }
 }
+
--- nvim/plugins/nvim-tree.vim
+++ nvim/plugins/nvim-tree.vim
@@ -1,9 +1,3 @@
-let g:nvim_tree_ignore = [
-    \'.git', 'node_modules', '.cache', '.idea',
-    \'.DS_Store', '.venv', '__pycache__', '.mypy_cache',
-    \'.pytest_cache', 'htmlcov', 'reports', '.hypothesis',
-    \'.coverage', 'coverage.xml',
-\]
 let g:lua_tree_indent_markers = 0
 let g:lua_tree_icons = {
     \ 'default': '',
